28c6665d857cb6e8cfdcb3f53459021e216b1b68
Dat <nguyenquocdat312000@gmail.com>
2022-09-13 01:17:38 +0700

    Refactor code : Routes,Controllers

--- src/index.js
+++ src/index.js
@@ -2,10 +2,21 @@ const express = require("express");
 const path = require("path");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars");
+const res = require("express/lib/response");
 const app = express();
-const port = 3000;
+const port = 5000;
+const routes = require("./routes");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
+// XMLHttpRequest. fetch,axios
+
 //HTTP logger
 app.use(morgan("combined"));
 
@@ -17,12 +28,9 @@ app.engine(
   })
 );
 app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources\\views"));
-
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
+app.set("views", path.join(__dirname, "/resources/views"));
+//router
+route(app);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
